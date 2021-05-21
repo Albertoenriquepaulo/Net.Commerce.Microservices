5a252d66ef0a0ebb73c33b8810588f12ef705e6a
Alberto Paulo <Albertopaulo@gmail.com>
2021-05-21 10:42:15 +0200

    Add project files.

---
+++ src/MicroServices/Catalog/Catalog.Persistence.Database/Configuration/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Catalog.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.Persistence.Database.Configuration
+{
+    public class ProductConfiguration
+    {
+        public ProductConfiguration(EntityTypeBuilder<Product> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasIndex(x => x.Id);
+            entityTypeBuilder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            entityTypeBuilder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+
+            //Seed - Products by default
+            var products = new List<Product>();
+            var random = new Random();
+
+            for (var i = 0; i < 101; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id = i,
+                    Name = $"Product {i}",
+                    Description = $"Description of product {i}",
+                    Price = random.Next(100, 1000),
+                });
+            }
+
+            entityTypeBuilder.HasData(products);
+
+        }
+
+
+    }
+}
---
+++ src/MicroServices/Catalog/Catalog.Persistence.Database/Configuration/ProductInStockConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain;
+
+namespace Catalog.Persistence.Database.Configuration
+{
+    public class ProductInStockConfiguration
+    {
+        public ProductInStockConfiguration(EntityTypeBuilder<ProductInStock> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasIndex(x => x.Id);
+
+            var products = new List<ProductInStock>();
+            var random = new Random();
+
+            for (var i = 0; i < 101; i++)
+            {
+                products.Add(new ProductInStock()
+                {
+                    Id = i,
+                    ProductId = i,
+                    Stock = random.Next(0, 20)
+                });
+            }
+
+            entityTypeBuilder.HasData(products);
+        }
+    }
+}
